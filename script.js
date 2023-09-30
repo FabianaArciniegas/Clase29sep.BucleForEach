baff0afea600014a18251e6cca93b2c89650b159
FabianaArciniegas <144750464+FabianaArciniegas@users.noreply.github.com>
2023-09-29 20:59:08 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-const letters = ["a", "b", "c", "d"];
+const nombres = ["antonio", "bibiana", "carlos", "diana"];
 
 // Funciones por expresion
-const f = function (letters) {
-  console.log("un elemento");
-};
-letters.forEach(f);
+console.log("From ForEach() function")
+nombres.forEach( function (currenttValue) {
+  console.log(currentValue);
+});
 
 // Funciones anonimas
 letters.forEach (function () {
@@ -12,5 +12,5 @@ letters.forEach (function () {
 });
 
 //Funciones flecha
-letters.forEach( () => console.log("un elemento"));
+letters.forEach( (element) => console.log(element));
 
