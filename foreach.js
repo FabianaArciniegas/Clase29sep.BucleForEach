2e1c4f73e55e31cf410d47e9b2e78b09e1eeae30
FabianaArciniegas <144750464+FabianaArciniegas@users.noreply.github.com>
2023-10-01 19:27:07 -0500

    Update foreach.js

--- foreach.js
+++ foreach.js
@@ -1,17 +1,17 @@
 
+const animales = ["oso", "perro", "gato", "loro", "caracol"]
 
 // con funcion expresion
-// const numeros = [1, 2, 3, 4, 5]
-// const lista = function (animal) {
-//   console.log (animal);
-// };
+const lista = function (animal) {
+  console.log (animal);
+};
+animales.forEach (lista);
 
 // // con funcion anomina
-// const animales = ["oso", "perro", "gato", "loro", "caracol"]
-// animales.foreach (function () {
-// console.log (animal);
-// });
+
+animales.forEach (function (animal) {
+console.log (animal);
+});
 
 // con funcion flecha
-const listaanimales = ["oso", "perro", "gato", "loro", "caracol"]
-listaanimales.foreach((listaanimal) => console.log (listaanimal));
+animales.forEach((listaanimal) => console.log (listaanimal));
